e950c5cd705a827e171610996de326b25b8127e1
AmirAli Ghafouri <115145112+amiralighafouri81@users.noreply.github.com>
2024-08-14 08:27:56 +0330

    Update spider.py

--- crawler/crawler/spiders/spider.py
+++ crawler/crawler/spiders/spider.py
@@ -1,7 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.exceptions import CloseSpider
-import pandas
 
 class CrawlerSpider(CrawlSpider):
     name = 'spider'
